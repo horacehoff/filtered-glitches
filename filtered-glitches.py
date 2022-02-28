542fed0f608607c5a885ebfda36889066d895676
Just_a_Mango <horace.hoff@gmail.com>
2022-02-28 18:18:30 +0100

    Added error support

--- filtered-glitches.py
+++ filtered-glitches.py
@@ -4,46 +4,51 @@ import os
 from os.path import isfile, join
 onlyfiles = [f for f in listdir(os.getcwd()) if isfile(join(os.getcwd(), f))]
 def nft(file):
-    print ("\033[A                                                     \033[A")
-    print("--------------------------\nFILTERED GLITCHES NFT GENERATOR\nBy @Just_a_Mango\n--------------------------")
-    print("Starting nft generation for '"+file+"'...")
-    enhancer = ImageEnhance.Sharpness(Image.open(file))
-    print("Starting image sharpening...")
-    enhancer = enhancer.enhance(50000)
-    im1 = enhancer.filter(ImageFilter.SHARPEN)
-    print("Image sharpening finished")
-    print("Starting image sharpening...")
-    import random
-    index = random.randint(5,5000)
-    i = 0
-    while i < index:
-        im1 = im1.filter(ImageFilter.SHARPEN)
-        print ("\033[A                             \033[A")
-        print("Image sharpening -> "+str(i)+" out of "+str(index)+" | "+str(round(((i*100)/index), 2))+"%")
-        i = i+1
-    i = 0
-    import random
-    index = random.randint(5,5000)
-    while i < index:
-        im1 = im1.filter(ImageFilter.BLUR)
-        print ("\033[A                             \033[A")
-        print("[SECOND PASS]Image sharpening -> "+str(i)+" out of "+str(index)+" | "+str(round(((i*100)/index), 2))+"%")
-        i = i+1
-    number = random.randint(1, 1000)
-    print("Saving first image... -> "+"outimg"+str(number)+".png")
-    im1.save("Images/outimg"+str(number)+".png")
-    print("Starting image 'bluring'...")
-    index = random.randint(5,5000)
-    i = 0
-    im2 = im1
-    while i < index:
-        im2 = im2.filter(ImageFilter.SHARPEN)
-        print ("\033[A                             \033[A")
-        print("Image 'blur' -> "+str(i)+" out of "+str(index)+" | "+str(round(((i*100)/index), 2))+"%")
-        i = i+1
-    number = random.randint(1, 1000)
-    print("Saving second image... -> "+"outimg"+str(number)+".png")
-    im2.save("Images/outimg"+str(number)+".png")
+    try:
+        print ("\033[A                                                     \033[A")
+        print("-------------------------------\nFILTERED GLITCHES NFT GENERATOR\nBy @Just_a_Mango\n-------------------------------")
+        print("Starting nft generation for '"+file+"'...")
+        enhancer = ImageEnhance.Sharpness(Image.open(file))
+        print("Starting image sharpening...")
+        enhancer = enhancer.enhance(50000)
+        im1 = enhancer.filter(ImageFilter.SHARPEN)
+        print("Image sharpening finished")
+        print("Starting image sharpening...")
+        import random
+        index = random.randint(5,5000)
+        i = 0
+        while i < index:
+            im1 = im1.filter(ImageFilter.SHARPEN)
+            print ("\033[A                             \033[A")
+            print("Image sharpening -> "+str(i)+" out of "+str(index)+" | "+str(round(((i*100)/index), 2))+"%")
+            i = i+1
+        i = 0
+        import random
+        index = random.randint(5,5000)
+        while i < index:
+            im1 = im1.filter(ImageFilter.BLUR)
+            print ("\033[A                             \033[A")
+            print("[SECOND PASS]Image sharpening -> "+str(i)+" out of "+str(index)+" | "+str(round(((i*100)/index), 2))+"%")
+            i = i+1
+        number = random.randint(1, 1000)
+        print("Saving first image... -> "+"outimg"+str(number)+".png")
+        im1.save("Images/outimg"+str(number)+".png")
+        print("Starting image 'bluring'...")
+        index = random.randint(5,5000)
+        i = 0
+        im2 = im1
+        while i < index:
+            im2 = im2.filter(ImageFilter.SHARPEN)
+            print ("\033[A                             \033[A")
+            print("Image 'blur' -> "+str(i)+" out of "+str(index)+" | "+str(round(((i*100)/index), 2))+"%")
+            i = i+1
+        number = random.randint(1, 1000)
+        print("Saving second image... -> "+"outimg"+str(number)+".png")
+        im2.save("Images/outimg"+str(number)+".png")
+    except KeyboardInterrupt:
+        print("[Keyboard Interrupt] Stopped execution")
+    except Exception as e:
+        print("[Error] -> "+str(e))
     
 while True:
     img_path = input("Desired image path: ")
